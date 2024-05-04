f77105fa01a24d1142dd307a677d8818b649003a
Ivan Sen <senivan1997@gmail.com>
2024-05-04 20:57:33 +0300

    Updated gitignore, changed key storage. Working on the message saving

--- Chat_app/server.py
+++ Chat_app/server.py
@@ -14,74 +14,58 @@ from Encryption_algos import ECC, RSA
 from server_utils import database
 
 class Keys:
-    def __init__(self, public_key, private_key):
-        self.public_key = public_key
-        self.private_key = private_key
-    
-    def get_public_key(self):
-        return self.public_key
-    
-    def get_private_key(self):
-        return self.private_key
     @staticmethod
-    def from_tuple(tup):
-        return Keys(tup[0], tup[1])
-    
-    def __str__(self):
-        return f"Public key: {self.public_key}\nPrivate key: {self.private_key}"
-    
-    def __getitem__(self, key):
-        if key == 0:
-            return self.public_key
-        elif key == 1:
-            return self.private_key
+    def get_public_key(protocol):
+        secret = json.loads(open("server_util/server_secret.json", "r").read())
+        if protocol not in secret.keys():
+            if protocol == "RSA":
+                pub, priv = RSA.generateRSAkeys()
+            if protocol == "ECC":
+                pub, priv = ECC.ECC.generate_keys()
+            secret[protocol]["public_key"] = pub
+            secret[protocol]["private_key"] = priv
+            with open("server_util/server_secret.json", "w") as file:
+                file.write(json.dumps(secret))
+            return pub
         else:
-            raise IndexError("Index out of range")
+            return secret[protocol]["public_key"]
+    @staticmethod
+    def get_private_key(protocol):
+        secret = json.loads(open("server_util/server_secret.json", "r").read())
+        if protocol not in secret.keys():
+            if protocol == "RSA":
+                pub, priv = RSA.generateRSAkeys()
+            if protocol == "ECC":
+                pub, priv = ECC.ECC.generate_keys()
+            secret[protocol]["public_key"] = pub
+            secret[protocol]["private_key"] = priv
+            with open("server_util/server_secret.json", "w") as file:
+                file.write(json.dumps(secret))
+            return priv
+        else:
+            return secret[protocol]["private_key"]
+
 class Config:
     def __init__(self, conf):
         self.encrypt = conf["encrypt"]
         self.db_path = conf["db_path"]
         self.host = conf["host"]
         self.port = conf["port"]
-        self.public_key_path = conf["public_key_path"]
-        self.private_key_path = conf["private_key_path"]
 
 class Server:
     def __init__(self, config:'Config'):
         self.config = config
         self.db = database.Database(self.config.db_path)
-        self.keys = Keys.from_tuple(self.get_keys())
-        print(f"Keys: {self.keys}")
         self.users = {}
-    
-    def get_keys(self):
-        if os.path.exists(self.config.public_key_path) and os.path.exists(self.config.private_key_path):
-            with open(self.config.public_key_path, "r", encoding='utf-8') as file:
-                public_key = json.loads(file.read())
-            with open(self.config.private_key_path, "r", encoding='utf-8') as file:
-                private_key = json.loads(file.read())
-            return (public_key, private_key)
-        else:
-            if self.config.encrypt == "ECC":
-                keys = ECC.ECC.generate_keys()
-            elif self.config.encrypt == "RSA":
-                keys = RSA.generateRSAkeys()
-            with open(self.config.public_key_path, "w") as file:
-                file.write(json.dumps(keys[0]))
-            with open(self.config.private_key_path, "w") as file:
-                if self.config.encrypt == "ECC":
-                    file.write(json.dumps(keys[1], cls=ECC.PointEncoder))
-                else:
-                    file.write(json.dumps(keys[1]))
-            return keys
-        
+        self.keys = (Keys.get_public_key(self.config.encrypt), Keys.get_private_key(self.config.encrypt))
 
     def run(self):
-        start_server = websockets.serve(self.connect, self.config.host, self.config.port)
+        start_server = websockets.serve(self.connect, self.config.host, self.config.port, ping_interval=30, ping_timeout=120)
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
     
     async def send_message(self, message):
+        message = json.dumps(message)
         for user in self.users:
             await user.send(RSA.encrypt(message, self.users[user][1]))
     
@@ -91,7 +75,7 @@ class Server:
                 message = await websocket.recv()
                 message = RSA.decrypt(message, self.keys[1])
                 message = json.loads(message)
-                if hashlib.sha256(message['data']).hexdigest() != message['hash']:
+                if hashlib.sha256(message['data'].encode('utf-8')).hexdigest() != message['hash']:
                     print("Message has been tampered with")
                     continue
                 self.db.create_message(message["data"], message["time_sent"], self.db.get_user_id(message["sender_username"]),0, message["type"], message["hash"])
