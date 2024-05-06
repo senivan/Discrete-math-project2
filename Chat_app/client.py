144262f0e889ee0a44d7dfaa5f0637393c67715d
Ivan Sen <senivan1997@gmail.com>
2024-05-06 20:43:32 +0300

    Added logo and updated pipfile

---
+++ Chat_app/client.py
@@ -0,0 +1,10 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = QMainWindow()
+    window.show()
+    app.exec()
+    
