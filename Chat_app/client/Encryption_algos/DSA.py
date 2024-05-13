0fdacb2a36f5439edae77e0fa907c58eb85e8daa
Ivan Sen <senivan1997@gmail.com>
2024-05-14 00:48:18 +0300

    dsa fix

--- Chat_app/client/Encryption_algos/DSA.py
+++ Chat_app/client/Encryption_algos/DSA.py
@@ -2,23 +2,23 @@ import hashlib
 from Encryption_algos import RSA
 
 class DSA:
-    def __init__(self, private_key, public_key):
-        self.private_key = private_key
-        self.public_key = public_key
-
-    def sign(self, message):
+    # def __init__(self, private_key, public_key):
+    #     self.private_key = private_key
+    #     self.public_key = public_key
+    @staticmethod
+    def sign(message, private_key):
         '''
         Function signs the data
         '''
         hashed_message = hashlib.sha256(message.encode('utf-8')).hexdigest()
-        sign_ = RSA.encrypt(hashed_message, self.private_key)
+        sign_ = RSA.encrypt(hashed_message, private_key)
         return sign_
-
-    def verify(self, message, signature):
+    @staticmethod
+    def verify(message, signature, public_key):
         '''
         The function verifies whether the signature is correct.
         '''
         hashed_message = hashlib.sha256(message.encode('utf-8')).hexdigest()
-        decrypted_signature = RSA.decrypt(signature, self.public_key)
+        decrypted_signature = RSA.decrypt(signature, public_key)
         return hashed_message == decrypted_signature
 
